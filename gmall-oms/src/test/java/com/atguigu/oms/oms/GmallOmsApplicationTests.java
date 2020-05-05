c1823755eac9cfdb2769cf488e3838ef419a4437
Ghost <guiyingmoyin@gmail.com>
2020-05-05 22:39:23 +0800

    项目结构创建完成

---
+++ gmall-oms/src/test/java/com/atguigu/oms/oms/GmallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.oms.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
